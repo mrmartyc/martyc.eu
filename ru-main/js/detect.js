347bb2d27642be4d238af6826184df33bdaac63b
martyc <101498713+mrmartyc@users.noreply.github.com>
2025-01-16 21:14:55 +0100

    Delete ru-main directory

--- ru-main/js/detect.js
+++
@@ -1,5 +0,0 @@
-if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    var script = document.createElement('script');
-    script.src = '/ru-main/js/tilt.js';
-    document.body.appendChild(script);
-}
